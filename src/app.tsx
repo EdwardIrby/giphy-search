4b384197f4c71cd786063a4785952adc9815ec34
Edward Irby <e.irby@pm.me>
2023-06-02 21:11:37 -0500

    @giphy/js-fetch-api has default export error when used with esm switching to rest

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,20 @@
 import { isle, PlaitProps, PlaitedElement, css } from 'plaited'
+import { GiphyFetch } from '@giphy/js-fetch-api'
+
+/**
+ * I'm importing the giphy fetch library here and exposing
+ * my API key. Normally I wouldn't do this but I'm trying to timebox
+ * the work on this 1-2 hours.
+ */
+const gf = new GiphyFetch('0LRue8eVT7QMC87k7mT8FkCDNAtJN1kC')
 
 export const App = isle(
   { tag: 'giphy-app' },
   base => class extends base {
-    async plait(props: PlaitProps): void | Promise<void> {
+    async plait(props: PlaitProps) {
 
     }
   }
 )
 
-export const AppTemplate: PlaitedElement = () => <App.template></App.template>
+export const AppTemplate: PlaitedElement = () => <App.template>Giphy App</App.template>
